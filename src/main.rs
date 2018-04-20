700f17dd304d7df4ab6a79758d8555fdbe8a0ccb
ashtneoi <ashtneoi@gmail.com>
2018-04-19 17:49:51 -0700

    Read lines with initial whitespace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-extern crate num;
-
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::process::exit;
 
 #[derive(Debug)]
@@ -38,13 +38,43 @@ fn process_args<'a>(args: &[&'a str])
     Ok((args[0], output_type))
 }
 
+fn read_initial_ws(filename: &str) -> io::Result<Vec<(i32, i32)>> {
+    let file = File::open(filename)?;
+    let mut lines = io::BufReader::new(file).lines();
+
+    for _ in 0..100 {
+        let maybe_line = (&mut lines).skip_while(|x| {
+            match x {
+                Ok(ref line) => !(
+                    line.starts_with('\t') || line.starts_with(' ')
+                ),
+                Err(_) => false,
+            }
+        }).next();
+        let line = match maybe_line {
+            Some(Ok(line)) => line,
+            Some(Err(e)) => return Err(e),
+            None => break,
+        };
+        println!("{}", &line);
+    }
+
+    Ok(vec![])
+}
+
 fn main() {
     let owned_args: Vec<_> = env::args().skip(1).collect();
     let args: Vec<_> = owned_args.iter().map(|s| s.as_str()).collect();
     let (filename, output_type) = process_args(&args)
         .unwrap_or_else(|e| {
-            eprintln!("{}", e);
+            eprintln!("error: {}", e);
             exit(2);
         });
     println!("{} -> {:?}", filename, output_type);
+
+    let initial_ws = read_initial_ws(&filename)
+        .unwrap_or_else(|e| {
+            eprintln!("error: {}", e);
+            exit(2);
+        });
 }
