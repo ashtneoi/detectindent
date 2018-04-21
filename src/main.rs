092c7914359f21910373bdfb87eb39b9fe5b2e11
ashtneoi <ashtneoi@gmail.com>
2018-04-20 18:22:47 -0700

    Rework basically everything

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,23 @@ use std::io::{self, BufRead};
 use std::process::exit;
 
 #[derive(Debug)]
-enum OutputType {
+enum OutputFormat {
     Generic,
     Vim,
 }
 
 fn exit_with_usage() -> ! {
     eprint!("\
-        Usage:  indentdetect FILE OUTPUT-TYPE INDENT-UNIT\n\
+        Usage:  indentdetect FILE FORMAT DEFTABWIDTH\n\
         \n\
-        OUTPUT-TYPE: output type (`vim` or `generic`)\n\
-        INDENT-UNIT: equivalent spaces per indent level\n\
+        FORMAT: output format (`vim` or `generic`)\n\
+        DEFTABWIDTH: default tab width\n\
     ");
     exit(2);
 }
 
 fn process_args<'a>(args: &[&'a str])
-    -> Result<(&'a str, OutputType, u32), String>
+    -> Result<(&'a str, OutputFormat, u32), String>
 {
     if args.len() == 0 {
         exit_with_usage();
@@ -35,26 +35,27 @@ fn process_args<'a>(args: &[&'a str])
         return Err("Too many arguments".to_string());
     }
 
-    let output_type = match args[1] {
-        "generic" => OutputType::Generic,
-        "vim" => OutputType::Vim,
-        _ => { return Err("Invalid output type".to_string()); },
+    let output_format = match args[1] {
+        "generic" => OutputFormat::Generic,
+        "vim" => OutputFormat::Vim,
+        _ => { return Err("Invalid output format".to_string()); },
     };
 
-    let unit = args[2].parse::<u32>()
-        .map_err(|_| "Invalid indent unit".to_string())?;
-    if unit == 0 {
-        return Err("Indent unit can't be zero".to_string());
+    let def_tab_width = args[2].parse::<u32>()
+        .map_err(|_| "Invalid default tab width".to_string())?;
+    if def_tab_width == 0 {
+        return Err("Default tab width can't be zero".to_string());
     }
 
-    Ok((args[0], output_type, unit))
+    Ok((args[0], output_format, def_tab_width))
 }
 
-fn count_indents(filename: &str) -> io::Result<Vec<(u32, u32)>> {
+fn count_indents(filename: &str) -> io::Result<(bool, Vec<u32>)> {
     let file = File::open(filename)?;
     let mut lines = io::BufReader::new(file).lines();
 
-    let mut counts = Vec::new();
+    let mut tabs = false;
+    let mut sp_counts = Vec::new();
 
     for _ in 0..100 {
         let maybe_line = (&mut lines).skip_while(|x| {
@@ -72,17 +73,18 @@ fn count_indents(filename: &str) -> io::Result<Vec<(u32, u32)>> {
         };
 
         let mut chars = line.chars().peekable();
-        let tab_count = peek_while(&mut chars, |&x| { x == '\t' })
-            .count() as u32;
+        if peek_while(&mut chars, |&x| { x == '\t' }).count() > 0 {
+            tabs = true;
+        }
         let sp_count = peek_while(&mut chars, |&x| { x == ' ' })
             .count() as u32;
         // TODO: chars.peek() == Some(&'\t') is probably an error. (Not sure
         // how best to return it, though.)
-        counts.push((tab_count, sp_count));
+        sp_counts.push(sp_count);
     }
-    assert!(counts.len() > 0); // TODO: not a bug
+    assert!(sp_counts.len() > 0); // TODO: not a bug
 
-    Ok(counts)
+    Ok((tabs, sp_counts))
 }
 
 fn maybe_gcd(x: u32, y: u32) -> u32 {
@@ -98,51 +100,43 @@ fn maybe_gcd(x: u32, y: u32) -> u32 {
     }
 }
 
-fn detect_indent(counts: &Vec<(u32, u32)>, unit: u32)
+fn detect_indent(tabs: bool, sp_counts: &[u32], def_tab_width: u32)
     -> Result<(u32, u32), String>
 {
-    let mut tab_ind = 0; // 0 means infinity, I guess
-    let mut sp_ind = 0;
-    for &(tab_count, sp_count) in counts {
-        tab_ind = maybe_gcd(tab_ind, unit*tab_count);
-        sp_ind = maybe_gcd(sp_ind, sp_count);
+    let mut sp_unit = 0; // 0 means infinity, I guess
+    let mut max_sp = 0;
+    for &sp_count in sp_counts {
+        sp_unit = maybe_gcd(sp_unit, sp_count);
+        max_sp = max_sp.max(sp_count);
     }
 
-    if sp_ind < unit {
-        // TODO: Use better words.
-        return Err("Space indent is narrower than indent unit".to_string());
-    }
-    if !sp_ind.is_multiple_of(&unit) {
-        // TODO: Use better words.
-        return Err("Space indent isn't a multiple of indent unit".to_string());
-    }
-    if tab_ind > 0 && 2 * sp_ind >= tab_ind {
-        // TODO: Use better words.
-        return Err("Space indent is too wide".to_string());
-    }
+    let tab_width = match (tabs, sp_unit) {
+        (true, 0) => def_tab_width,
+        (true, _) => max_sp + sp_unit, // TODO: this is unreliable
+        (false, _) => 0,
+    };
 
-    Ok((tab_ind, sp_ind))
+    Ok((tab_width, sp_unit))
 }
 
-fn format_indent((tab_ind, sp_ind): (u32, u32), output_type: OutputType)
+fn format_indent((tab_width, sp_unit): (u32, u32), output_format: OutputFormat)
     -> String
 {
-    assert!(!(tab_ind == 0 && sp_ind == 0));
-    // TODO: Don't assume tab+space tab width is 2*unit.
-    match output_type {
-        OutputType::Generic => {
-            let (kind, count) = match (tab_ind, sp_ind) {
-                (ti, 0) => ("tab", ti.to_string()),
-                (0, si) => ("space", si.to_string()),
-                (ti, si) => ("tab+space", format!("{} {}", 2*ti, si)),
+    assert!(!(tab_width == 0 && sp_unit == 0));
+    match output_format {
+        OutputFormat::Generic => {
+            let (kind, count) = match (tab_width, sp_unit) {
+                (t, 0) => ("tab", t.to_string()),
+                (0, s) => ("space", s.to_string()),
+                (t, s) => ("tab+space", format!("{} {}", t, s)),
             };
             format!("{} {}", kind, count)
         },
-        OutputType::Vim => {
-            let (expandtab, tabstop, shiftwidth) = match (tab_ind, sp_ind) {
-                (ti, 0) => (false, ti, ti),
-                (0, si) => (true, si, si),
-                (ti, si) => (false, 2*ti, si),
+        OutputFormat::Vim => {
+            let (expandtab, tabstop, shiftwidth) = match (tab_width, sp_unit) {
+                (t, 0) => (false, t, t),
+                (0, s) => (true, s, s),
+                (t, s) => (false, t, s),
             };
             format!(
                 "set {}expandtab tabstop={} shiftwidth={}",
@@ -157,23 +151,23 @@ fn format_indent((tab_ind, sp_ind): (u32, u32), output_type: OutputType)
 fn main() {
     let owned_args: Vec<_> = env::args().skip(1).collect();
     let args: Vec<_> = owned_args.iter().map(|s| s.as_str()).collect();
-    let (filename, output_type, unit) = process_args(&args)
+    let (filename, output_format, def_tab_width) = process_args(&args)
         .unwrap_or_else(|e| {
             eprintln!("error: {}", e);
             exit(2);
         });
 
-    let counts = count_indents(&filename)
+    let (tabs, sp_counts) = count_indents(&filename)
         .unwrap_or_else(|e| {
             eprintln!("error: {}", e);
             exit(1);
         });
 
-    let indent = detect_indent(&counts, unit)
+    let indent = detect_indent(tabs, &sp_counts, def_tab_width)
         .unwrap_or_else(|e| {
             eprintln!("error: {}", e);
             exit(1);
         });
 
-    println!("{}", format_indent(indent, output_type));
+    println!("{}", format_indent(indent, output_format));
 }
